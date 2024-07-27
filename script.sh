bb19c9686c9d536d255ee59d379a4fd107895bcc
byinjekt <127022651+byinjekt@users.noreply.github.com>
2024-07-27 20:44:28 +0700

    added script

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+    exit 1
+fi
+
+filename=$1
+extension=$2
+
+base_name="${filename%.*}"
+
+#Проверка на наличие расширения у файла
+if [ "$base_name" == "$filename" ]; then
+    new_filename="${filename}.${extension}"
+else
+    new_filename="${base_name}.${extension}"
+fi
+
+mv "$filename" "$new_filename"
